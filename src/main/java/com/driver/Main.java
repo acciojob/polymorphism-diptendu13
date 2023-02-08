b95cf577c5102abafc7ad7c3864cbd0061374958
diptendu <mandaldiptendu99@gmail.com>
2023-02-08 19:47:19 +0530

    First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
+import java.nio.charset.MalformedInputException;
+import java.util.*;
 public class Main {
-
+    static class Product{
+        public int product(int x, int y) {
+          return 1;
+        }
+        public int product(int x, int y, int z) {
+            return 2;
+        }
+        public double product(double x, double y) {
+            return 3.0;
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Product p = new Product();
+        p.product(sc.nextInt(), sc.nextInt());
+        p.product(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        p.product(sc.nextDouble(), sc.nextDouble());
+    }
 }
